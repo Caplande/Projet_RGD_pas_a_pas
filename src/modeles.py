8e9c267008ed737910121b25411d144a013965bb
Caplande <georges.caplande@gmail.com>
2025-09-28 09:14:17 +0200

    Synchronisation du module modeles.py avec la bdd mise aux normes PEP8

--- src/modeles.py
+++ src/modeles.py
@@ -1,8 +1,7 @@
 from typing import Optional
-from sqlalchemy import BigInteger, Boolean, Float, Index, Integer, Text, String, Date
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from datetime import date
 
+from sqlalchemy import BigInteger, Boolean, Float, Index, Integer, REAL, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
     pass
@@ -12,29 +11,29 @@ class TAgregation(Base):
     __tablename__ = 't_agregation'
 
     id: Mapped[Optional[int]] = mapped_column(Integer, primary_key=True)
-    type_appel: Mapped[Optional[str]] = mapped_column(String(2))
+    type_appel: Mapped[Optional[str]] = mapped_column(Text(2))
     libelle1: Mapped[Optional[str]] = mapped_column(Text)
-    debut_periode: Mapped[date] = mapped_column(Date)
-    fin_periode: Mapped[date] = mapped_column(Date)
     periode_cloturee: Mapped[Optional[str]] = mapped_column(Text)
-    bat: Mapped[Optional[str]] = mapped_column(String(3))
-    bat_tit: Mapped[Optional[str]] = mapped_column(String(50))
-    rub: Mapped[Optional[str]] = mapped_column(String(2))
-    rub_tit: Mapped[Optional[str]] = mapped_column(String(50))
-    typ: Mapped[Optional[str]] = mapped_column(String(3))
-    typ_tit: Mapped[Optional[str]] = mapped_column(String(50))
-    date_a: Mapped[date] = mapped_column(Date)
-    libelle: Mapped[Optional[str]] = mapped_column(String(50))
-    reference: Mapped[Optional[str]] = mapped_column(String(50))
-    montant: Mapped[Optional[float]] = mapped_column(Float)
-    nom_fournisseur: Mapped[Optional[str]] = mapped_column(String(50))
+    bat: Mapped[Optional[str]] = mapped_column(Text(3))
+    bat_tit: Mapped[Optional[str]] = mapped_column(Text(50))
+    rub: Mapped[Optional[str]] = mapped_column(Text(2))
+    rub_tit: Mapped[Optional[str]] = mapped_column(Text(50))
+    typ: Mapped[Optional[str]] = mapped_column(Text(3))
+    typ_tit: Mapped[Optional[str]] = mapped_column(Text(3))
+    libelle: Mapped[Optional[str]] = mapped_column(Text(50))
+    reference: Mapped[Optional[str]] = mapped_column(Text(50))
+    montant_a_repartir: Mapped[Optional[float]] = mapped_column(Float)
+    nom_fournisseur: Mapped[Optional[str]] = mapped_column(Text(50))
+    debut_periode: Mapped[Optional[float]] = mapped_column(REAL)
+    fin_periode: Mapped[Optional[float]] = mapped_column(REAL)
+    date_a: Mapped[Optional[float]] = mapped_column(REAL)
 
 
 class TClesRepartition(Base):
     __tablename__ = 't_cles_repartition'
 
     id: Mapped[Optional[int]] = mapped_column(Integer, primary_key=True)
-    base_rep: Mapped[Optional[str]] = mapped_column(String(20))
+    base_rep: Mapped[Optional[str]] = mapped_column(Text(20))
     E: Mapped[Optional[float]] = mapped_column(Float)
     R: Mapped[Optional[float]] = mapped_column(Float)
     G: Mapped[Optional[float]] = mapped_column(Float)
@@ -49,64 +48,57 @@ class TData(Base):
     __tablename__ = 't_data'
 
     id: Mapped[Optional[int]] = mapped_column(Integer, primary_key=True)
-    type_appel: Mapped[Optional[str]] = mapped_column(String(2))
+    type_appel: Mapped[Optional[str]] = mapped_column(Text(2))
     libelle1: Mapped[Optional[str]] = mapped_column(Text)
-    debut_periode: Mapped[date] = mapped_column(Date)
-    fin_periode: Mapped[date] = mapped_column(Date)
     periode_cloturee: Mapped[Optional[str]] = mapped_column(Text)
-    bat: Mapped[Optional[str]] = mapped_column(String(3))
-    bat_tit: Mapped[Optional[str]] = mapped_column(String(50))
-    rub: Mapped[Optional[str]] = mapped_column(String(2))
-    rub_tit: Mapped[Optional[str]] = mapped_column(String(50))
-    typ: Mapped[Optional[str]] = mapped_column(String(3))
-    typ_tit: Mapped[Optional[str]] = mapped_column(String(50))
-    date_a: Mapped[date] = mapped_column(Date)
-    libelle: Mapped[Optional[str]] = mapped_column(String(50))
-    reference: Mapped[Optional[str]] = mapped_column(String(50))
-    montant: Mapped[Optional[float]] = mapped_column(Float)
-    nom_fournisseur: Mapped[Optional[str]] = mapped_column(String(50))
+    bat: Mapped[Optional[str]] = mapped_column(Text(3))
+    bat_tit: Mapped[Optional[str]] = mapped_column(Text(50))
+    rub: Mapped[Optional[str]] = mapped_column(Text(2))
+    rub_tit: Mapped[Optional[str]] = mapped_column(Text(50))
+    typ: Mapped[Optional[str]] = mapped_column(Text(3))
+    typ_tit: Mapped[Optional[str]] = mapped_column(Text(50))
+    libelle: Mapped[Optional[str]] = mapped_column(Text(50))
+    reference: Mapped[Optional[str]] = mapped_column(Text(50))
+    montant_a_repartir: Mapped[Optional[float]] = mapped_column(Float)
+    nom_fournisseur: Mapped[Optional[str]] = mapped_column(Text(50))
+    debut_periode: Mapped[Optional[float]] = mapped_column(REAL)
+    fin_periode: Mapped[Optional[float]] = mapped_column(REAL)
+    date_a: Mapped[Optional[float]] = mapped_column(REAL)
 
 
 class TLexiqueBat(Base):
     __tablename__ = 't_lexique_bat'
 
     id: Mapped[Optional[int]] = mapped_column(Integer, primary_key=True)
-    bat: Mapped[Optional[str]] = mapped_column(String(3))
-    bat_tit: Mapped[Optional[str]] = mapped_column(String(50))
+    bat: Mapped[Optional[str]] = mapped_column(Text(3))
+    bat_tit: Mapped[Optional[str]] = mapped_column(Text(50))
 
 
 class TLexiqueBatrub(Base):
     __tablename__ = 't_lexique_batrub'
 
     id: Mapped[Optional[int]] = mapped_column(Integer, primary_key=True)
-    batrub: Mapped[Optional[str]] = mapped_column(String(6))
-    batrub_tit: Mapped[Optional[str]] = mapped_column(String(50))
-    base_rep: Mapped[Optional[str]] = mapped_column(String(20))
-    entites: Mapped[Optional[str]] = mapped_column(String(30))
-    rgpt_entites: Mapped[Optional[str]] = mapped_column(String(30))
-
-
-class TLexiqueCles(Base):
-    __tablename__ = 't_lexique_cles'
-
-    cle: Mapped[Optional[str]] = mapped_column(Text, primary_key=True)
-    groupe: Mapped[Optional[str]] = mapped_column(String(50))
+    batrub: Mapped[Optional[str]] = mapped_column(Text(6))
+    batrub_tit: Mapped[Optional[str]] = mapped_column(Text(50))
+    base_rep: Mapped[Optional[str]] = mapped_column(Text(20))
+    entites: Mapped[Optional[str]] = mapped_column(Text(30))
+    rgpt_entites: Mapped[Optional[str]] = mapped_column(Text(30))
 
 
 class TLexiqueRub(Base):
     __tablename__ = 't_lexique_rub'
 
     id: Mapped[Optional[int]] = mapped_column(Integer, primary_key=True)
-    rub: Mapped[Optional[str]] = mapped_column(String(2))
-    rub_tit: Mapped[Optional[str]] = mapped_column(String(50))
+    rub: Mapped[Optional[str]] = mapped_column(Text(2))
+    rub_tit: Mapped[Optional[str]] = mapped_column(Text(50))
 
 
 class TLexiqueTyp(Base):
     __tablename__ = 't_lexique_typ'
 
     id: Mapped[Optional[int]] = mapped_column(Integer, primary_key=True)
-    typ: Mapped[Optional[str]] = mapped_column(String(3))
-    typ_tit: Mapped[Optional[str]] = mapped_column(String(50))
+    typ: Mapped[Optional[str]] = mapped_column(Text(3))
+    typ_tit: Mapped[Optional[str]] = mapped_column(Text(50))
     typ_ambigu: Mapped[Optional[bool]] = mapped_column(Boolean)
 
 
@@ -114,7 +106,7 @@ class TListeGroupes(Base):
     __tablename__ = 't_liste_groupes'
 
     id: Mapped[Optional[int]] = mapped_column(Integer, primary_key=True)
-    groupe: Mapped[Optional[str]] = mapped_column(String(50))
+    groupe: Mapped[Optional[str]] = mapped_column(Text(50))
     recurrent: Mapped[Optional[int]] = mapped_column(BigInteger)
 
 
@@ -125,22 +117,22 @@ class TOriginelCompletee(Base):
     )
 
     id: Mapped[Optional[int]] = mapped_column(BigInteger, primary_key=True)
-    cle: Mapped[Optional[str]] = mapped_column(Text)
-    exercice: Mapped[Optional[str]] = mapped_column(String(4))
-    periode_cloturee: Mapped[Optional[str]] = mapped_column(String(1))
-    bat: Mapped[Optional[str]] = mapped_column(String(3))
-    rub: Mapped[Optional[str]] = mapped_column(String(2))
-    typ: Mapped[Optional[str]] = mapped_column(String(3))
-    typ_tit: Mapped[Optional[str]] = mapped_column(String(50))
-    batrubtyp: Mapped[Optional[str]] = mapped_column(String(10))
-    batrub: Mapped[Optional[str]] = mapped_column(String(6))
-    date_a: Mapped[date] = mapped_column(Date)
-    libelle: Mapped[Optional[str]] = mapped_column(String(50))
-    reference: Mapped[Optional[str]] = mapped_column(String(50))
+    cle: Mapped[Optional[str]] = mapped_column(Text(150))
+    exercice: Mapped[Optional[str]] = mapped_column(Text(4))
+    periode_cloturee: Mapped[Optional[str]] = mapped_column(Text(1))
+    bat: Mapped[Optional[str]] = mapped_column(Text(3))
+    rub: Mapped[Optional[str]] = mapped_column(Text(2))
+    typ: Mapped[Optional[str]] = mapped_column(Text(3))
+    typ_tit: Mapped[Optional[str]] = mapped_column(Text(50))
+    batrubtyp: Mapped[Optional[str]] = mapped_column(Text(8))
+    batrub: Mapped[Optional[str]] = mapped_column(Text(5))
+    date_a: Mapped[Optional[str]] = mapped_column(Text(10))
+    libelle: Mapped[Optional[str]] = mapped_column(Text(50))
+    reference: Mapped[Optional[str]] = mapped_column(Text(50))
     montant1: Mapped[Optional[float]] = mapped_column(Float)
-    nom_fournisseur: Mapped[Optional[str]] = mapped_column(String(50))
-    rang_doublon: Mapped[Optional[str]] = mapped_column(String(2))
-    groupe: Mapped[Optional[str]] = mapped_column(String(50))
+    nom_fournisseur: Mapped[Optional[str]] = mapped_column(Text(50))
+    rang_doublon: Mapped[Optional[str]] = mapped_column(Text(2))
+    groupe: Mapped[Optional[str]] = mapped_column(Text(50))
     montant: Mapped[Optional[float]] = mapped_column(Float)
 
 
@@ -148,5 +140,5 @@ class TParametres(Base):
     __tablename__ = 't_parametres'
 
     id: Mapped[Optional[int]] = mapped_column(Integer, primary_key=True)
-    indicateur: Mapped[Optional[str]] = mapped_column(String(50))
-    valeur: Mapped[Optional[str]] = mapped_column(String(20))
+    indicateur: Mapped[Optional[str]] = mapped_column(Text(50))
+    valeur: Mapped[Optional[str]] = mapped_column(Text(50))
